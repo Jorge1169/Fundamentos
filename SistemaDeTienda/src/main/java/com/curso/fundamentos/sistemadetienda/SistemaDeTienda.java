bf58fa2c56b960993055311ccd09c39628c215ab
Jorge1169 <127150143+Jorge1169@users.noreply.github.com>
2026-06-03 13:39:24 -0600

    Requerimientos
    Requerimientos del sistema

--- SistemaDeTienda/src/main/java/com/curso/fundamentos/sistemadetienda/SistemaDeTienda.java
+++ SistemaDeTienda/src/main/java/com/curso/fundamentos/sistemadetienda/SistemaDeTienda.java
@@ -11,6 +11,6 @@ package com.curso.fundamentos.sistemadetienda;
 public class SistemaDeTienda {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Mi tienda");
     }
 }
