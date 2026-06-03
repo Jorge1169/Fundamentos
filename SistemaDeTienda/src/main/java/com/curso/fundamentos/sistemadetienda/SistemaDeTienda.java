b224e1e20e3eae10952ee8b7dfa342ca406d602d
Jorge1169 <127150143+Jorge1169@users.noreply.github.com>
2026-06-03 11:02:19 -0600

    Mi tienda versión 1.0
    ////
    Se inicia proceso tiendita.

---
+++ SistemaDeTienda/src/main/java/com/curso/fundamentos/sistemadetienda/SistemaDeTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.curso.fundamentos.sistemadetienda;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SistemaDeTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
